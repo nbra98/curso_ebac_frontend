7a306975e87b617896df46b73eb8874fa581448d
Luis Fernando <117687568+nbra98@users.noreply.github.com>
2023-07-25 18:03:32 -0300

    fix: algorithm script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let form = document.querySelector("#form-validation");
-let numberA = document.querySelector("#number-a");
-let numberB = document.querySelector("#number-b");
 const sucessMessage = document.querySelector(".sucess-message");
 const errorMessage = document.querySelector(".error-message");
 const sameNumberMessage = document.querySelector(".same-number-message");
@@ -8,21 +6,24 @@ const sameNumberMessage = document.querySelector(".same-number-message");
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  if (numberA.value > numberB.value) {
-    errorMessage.style.display = "block";
+  let numberA = Number(document.querySelector("#number-a").value);
+  let numberB = Number(document.querySelector("#number-b").value);
+
+  if (numberA > numberB) {
+    errorMessage.style.display = "block"; 
     
     sucessMessage.style.display = "none";
     sameNumberMessage.style.display = "none";
   } 
 
-  else if (numberA.value < numberB.value) {
+  else if (numberA < numberB) {
     sucessMessage.style.display = "block";
 
     errorMessage.style.display = "none";
     sameNumberMessage.style.display = "none";
   } 
   
-  else if (numberA.value === numberB.value) {
+  else if (numberA === numberB) {
     sameNumberMessage.style.display = "block";
 
     sucessMessage.style.display = "none";
