cc1be014b8c72e1660753af9ac5dd4897bd6a56d
Luis Fernando <117687568+nbra98@users.noreply.github.com>
2023-12-06 21:21:04 -0300

    Add files via upload

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    less: {
+      production: {
+        options: {
+          compress: true
+        },
+        files: {
+          'dist/styles/main.min.css': 'src/styles/main.less'
+        }
+      }
+    },
+
+    clean: ['prebuild'],
+
+    uglify: {
+      target: {
+        files: {
+          'dist/scripts/main.min.js': 'src/scripts/main.js'
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default');
+  grunt.registerTask('build', ['less', 'clean', 'uglify']);
+}
